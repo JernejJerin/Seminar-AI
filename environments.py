3054960a1bc481691d4744e936abf1b75fa055b0
Jernej Jerin <jernej.jerin@gmail.com>
2014-01-01 13:45:50 +0100

    Overview the implementation. Very good work! I added comments for myself (explainig to myself) and reply for TODO to check for position in corners.

--- environments.py
+++ environments.py
@@ -20,9 +20,18 @@ class Sokoban(Environment):
 				 stonePosList=[]):
 		self.size = size
 		self.stonePosSet = set(stonePosList) # optimizes search
+
+        # Start position is our position + positions of boxes.
+        # Position is define for dynamic objects, in this case agent and boxes.
 		self.startPos = (agentPos, ) + tuple(boxPosList)
+
+        # Terminal positions or positions to which the boxes need to be moved.
 		self.endPosSet = set(endPosList)
+
+        # Hole?
 		self.holePosSet = set(holePosList)
+
+        # Using notation of first columns then rows.
 		self.possibleActions = [
 			(0, +1), # up
 			(0, -1), # down
@@ -36,15 +45,24 @@ class Sokoban(Environment):
 			"right": self.possibleActions[3],
 		}
 
+        # Count for end position.
 		self.endOnEdgeCount = sum(self._on_edge(loc) for loc in self.endPosSet)
 
 	def printState(self, state):
+        # In the beginning state is self.startPos!
 		agentPos = state[0]
 		boxPosSet = set(state[1:]) # set optimizes search
+
+        # Print columns before last row.
 		print "|" + "".join("-" for i in range(self.size[0])) + "|"
+
+        # Over columns.
 		for j in range(self.size[0]):
 			line = "|"
+
+            # Over rows.
 			for i in range(self.size[1]):
+                # Change column and row order.
 				pt = (i, self.size[1] - 1 - j)
 				if pt == agentPos:
 					line += "A"
@@ -59,6 +77,8 @@ class Sokoban(Environment):
 				else:
 					line += " "
 			print line + "|"
+
+        # Print columns after last row.
 		print "|" + "".join("-" for i in range(self.size[0])) + "|"
 
 
@@ -66,25 +86,28 @@ class Sokoban(Environment):
 		return self.startPos
 
 	def do(self, state, action):
+        # Select appropriate action given notation.
 		if action in self.possibleActionsDict:
 			action = self.possibleActionsDict[action]
+
 		agentPos = state[0]
 		boxPosSet = set(state[1:]) # set optimizes search
 		newPos = self._add(agentPos, action) # get new position with respect to action
 		boxList = [] # new box positions
 		reward = -10 # agent reward
 		isTerminalState = False
-		
+
+		# After player move check positions for boxes.
 		for boxPos in boxPosSet:
 			newBoxPos = boxPos
 			# check if new position moves a box
 			if boxPos == newPos:
+                # Move box.
 				newBoxPos = self._add(boxPos, action)
 				reward = 30
-			if boxPos in self.endPosSet:
-				boxInEndPosCount += 1
 			boxList.append(newBoxPos)
 
+        # How many boxes are in end position and on edge of environment?
 		boxInEndPosCount = sum(box in self.endPosSet for box in boxList)
 		boxOnEdgeCount = sum(self._on_edge(loc) for loc in boxList)
 		# Check if the number of boxes on edges
@@ -93,6 +116,10 @@ class Sokoban(Environment):
 			# When a box is on the edge it cannot be moved to the center
 			# and that is why the game is over
 			# TODO: Jernej, check if this is a fact
+            # This is true. You cannot move the box away from edge but you can move it along the edge.
+            # And this of course only applies if you are not at the corner. Then you are stuck.
+            # TODO: But we should check for every edge (four edges) if the number of
+            # TODO: end positions for each edge does not exceed the number of boxes in each edge.
 			reward = -1000
 			isTerminalState = True
 
@@ -100,20 +127,26 @@ class Sokoban(Environment):
 		if boxInEndPosCount == len(self.endPosSet):
 			reward = 1000
 			isTerminalState = True
+        # First position is new position of a player.
 		return ((newPos, ) + tuple(boxList), 
 				reward, 
 				isTerminalState, )
 
-	def _in_borders(self, action):
-		return 0 <= action[0] < self.size[0] and 0 <= action[1] < self.size[1]
+    # Inside borders of box. Rename to location for clarity.
+	def _in_borders(self, loc):
+		return 0 <= loc[0] < self.size[0] and 0 <= loc[1] < self.size[1]
 
 	def _on_edge(self, loc):
 		"""
 		Check if location loc=(x,y) is on environment edge
 		"""
+
+        # Columns and rows count starts at 0!
 		return sum((loc[i] in {0, self.size[i]-1}) for i in range(2)) > 0
 
 	def _add(self, absolute, relative):
+        # Absolute = agent position or box position, relative = executed action (i.e. (0, +1))
+        # Checking if agent moves out of the box is done outside.
 		return (absolute[0] + relative[0], absolute[1] + relative[1])
 
 	def getActions(self, state):
@@ -122,12 +155,16 @@ class Sokoban(Environment):
 		
 		# check if possible
 		actions = []
+
+        # Possible actions (0, +1), (0, -1), etc.
 		for diff in self.possibleActions:
 			newPos = self._add(agentPos, diff)
 			if not self._in_borders(newPos) or newPos in self.stonePosSet:
 				continue
 			if newPos in boxPosSet: # we are moving a box
 				newBoxPos = self._add(newPos, diff)
+
+                # Remove position of the moved box. The position matches the new position of agent.
 				otherBoxes = boxPosSet - {newPos, } # get other boxes
 				if newBoxPos in otherBoxes:
 					# you cannot move two boxes in same time
@@ -136,6 +173,7 @@ class Sokoban(Environment):
 					continue
 			# everything is OK
 			actions.append(diff) # TODO: check if absolute actions are better
+        # Returns all possible actions for current state of environment.
 		return actions
 	
 
