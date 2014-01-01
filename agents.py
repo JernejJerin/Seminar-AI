bff6d01c494cd1efe42ff113fd07be773572115f
Jernej Jerin <jernej.jerin@gmail.com>
2014-01-01 20:10:24 +0100

    Added comments for myself to agent (not commented fully, yet). In environments implemented corner detection then realised that we really need full deadlock detection in that section. Check links and a solution in comments that I have found for deadlock detecion.

--- agents.py
+++ agents.py
@@ -1,36 +1,126 @@
 import random
 
+
 def _f2p(fList):
-	"""
+    """
 	Private method for calculating probabilities for
 	each value in a given list fList
 	"""
-	fDict = {}
-	n = len(fList)
-	for val in fList:
-		# calculate frequencies for each value
-		pDict.setdefault(val, 0)
-		pDict[val] += 1
-	# return probabilities
-	return dict((val, freq/n) for val, freq in fDict)
+    pDict = {}
+    n = len(fList)
+    for val in fList:
+        # calculate frequencies for each value
+        pDict.setdefault(val, 0)
+        pDict[val] += 1
+    # return probabilities
+    return dict((val, freq / n) for val, freq in pDict)
+
 
 def _alpha(n):
-	return 60. / (59 + n)
+    """
+    The step size function to ensure convergence. The
+    function decreases as the number of times a state
+    has been visited increases (n). It means that the
+    utility of policy pi for state s will converge to
+    correct value.
+    """
+    # TODO: This is used in Temporal-difference learning!
+    return 60. / (59 + n)
+
 
 def _getEstimates(transs, utils, currState, currActions=None):
-	# Return (rewardEstimate, action) pairs in a dict
-	estimates = []
-	for ac in (currActions or transs.get(currState, {})):
-		probs = transs.get(currState, {}).get(ac, {})
-		n = sum(val for val in probs.values())
-		probs = dict((key, val/n) for key, val in probs.iteritems())
-		estimates.append((sum(p*utils.get(s, 0) for s, p in probs.iteritems()), ac, ))
-	return estimates
+    """
+    Gets estimates according to current transition states,
+    utility, current state and actions that can be executed
+    in current state.
+
+    Return (rewardEstimate, action) pairs in a dict
+    """
+
+    estimates = []
+    for ac in (currActions or transs.get(currState, {})):
+        probs = transs.get(currState, {}).get(ac, {})
+        n = sum(val for val in probs.values())
+        probs = dict((key, val / n) for key, val in probs.iteritems())
+        estimates.append((sum(p * utils.get(s, 0) for s, p in probs.iteritems()), ac, ))
+    return estimates
 
 
-def adp_random_exploration(env, transs={}, utils={}, freqs={}, 
-						   t = 1, tStep=0.02, alpha=_alpha, maxItr=100000):
+def adp_random_exploration(env, transs={}, utils={}, freqs={},
+                           t=1, tStep=0.02, alpha=_alpha, maxItr=100000):
+    """
+	Active ADP (adaptive dynamic programming) learning
+	algorithm which returns the best policy for a given
+	environment env and experience dictionary exp
+	
+	The experience dictionary exp can be empty if 
+	the agent has no experience with the environment
+	but can also be full with values from
+	previous trials
+
+	The algorithm returns the number of iterations
+	needed to reach a terminal state
+
+	env - Environment
+	transs - Transition table
+	utils - Utilities table
+	freqs - Frequency table
+	t -
+	tStep -
+	alpha -
+	maxItr -
 	"""
+    itr = 0
+    isTerminal = False
+    state = env.getStartingState()
+
+    # Get possible actions with respect to current state.
+    actions = env.getActions(state)
+    rewardEstimate, bestAction = None, None
+    if len(utils) > 0: # if this is not the first trial
+        rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
+
+    while not isTerminal: # while not terminal
+        utility = None
+        if random.random() < 1. / t or bestAction is None:
+            # If it is the first iteration or exploration event
+            # then randomly choose an action
+            bestAction = random.choice(actions)
+
+        # do the action with the best policy
+        # or do some random exploration
+
+        newState, reward, isTerminal = env.do(state, bestAction)
+        freqs.setdefault(newState, 0)
+        freqs[newState] += 1
+
+        env.printState(state)
+        print actions, bestAction, isTerminal
+        env.printState(newState)
+        print "----------"
+        print "----------"
+        print "----------"
+
+        # update transition table
+        transs.setdefault(state, {}).setdefault(bestAction, {}).setdefault(newState, 0)
+        transs[state][bestAction][newState] += 1
+
+        actions = env.getActions(newState)
+        rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
+
+        # Update utility
+        utils[state] = reward + _alpha(freqs.get(state, 0)) * rewardEstimate
+
+        state = newState
+        t, itr = t + tStep, itr + 1
+        if itr >= maxItr:
+            break
+    return itr
+
+
+def adp_random_exploration_state(env, transs={}, utils={}, freqs={},
+                                 alpha=_alpha, maxItr=100000):
+    """
 	Active ADP learning algorithm which returns the best
 	policy for a given environment env and experience
 	dictionary exp
@@ -43,151 +133,101 @@ def adp_random_exploration(env, transs={}, utils={}, freqs={},
 	The algorithm returns the number of iterations
 	needed to reach a terminal state
 	"""
-	itr = 0
-	isTerminal = False
-	state = env.getStartingState()
-	
+    itr = 0
+    isTerminal = False
+    state = env.getStartingState()
 
-	actions = env.getActions(state)
-	rewardEstimate, bestAction = None, None
-	if len(utils) > 0: # if this is not the first trial
-		rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
+    actions = env.getActions(state)
+    rewardEstimate, bestAction = None, None
+    if len(utils) > 0: # if this is not the first trial
+        rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
 
-	while not isTerminal: # while not terminal
-		utility = None
-		if random.random() < 1./t or bestAction is None:
-			# If it is the first iteration or exploration event
-			# then randomly choose an action
-			bestAction = random.choice(actions)
-		
-		# do the action with the best policy
-		# or do some random exploration
-		
-		newState, reward, isTerminal = env.do(state, bestAction)
-		freqs.setdefault(newState, 0)
-		freqs[newState] += 1
+    while not isTerminal: # while not terminal
+        t = len(freqs) or 1
+        if random.random() < 1. / t or bestAction is None:
+            # If it is the first iteration or exploration event
+            # then randomly choose an action
+            bestAction = random.choice(actions)
 
-		env.printState(state)
-		print actions, bestAction, isTerminal
-		env.printState(newState)
-		print "----------"
-		print "----------"
-		print "----------"
+        # do the action with the best policy
+        # or do some random exploration
 
-		# update transition table
-		transs.setdefault(state, {}).setdefault(bestAction, {}).setdefault(newState, 0)
-		transs[state][bestAction][newState] += 1
+        newState, reward, isTerminal = env.do(state, bestAction)
+        freqs.setdefault(newState, 0)
+        freqs[newState] += 1
 
-		actions = env.getActions(newState)
-		rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
-		
-		# Update utility
-		utils[state] = reward + _alpha(freqs.get(state, 0)) * rewardEstimate
-		
-		state = newState
-		t, itr = t+tStep, itr+1
-		if itr >= maxItr:
-			break
-	return itr
+        env.printState(state)
+        print actions, bestAction, isTerminal
+        env.printState(newState)
+        print "----------"
+        print "----------"
+        print "----------"
 
-def adp_random_exploration_state(env, transs={}, utils={}, freqs={}, 
-						   alpha=_alpha, maxItr=100000):
-	"""
-	Active ADP learning algorithm which returns the best
-	policy for a given environment env and experience
-	dictionary exp
-	
-	The experience dictionary exp can be empty if 
-	the agent has no experience with the environment
-	but can also be full with values from
-	previous trials
+        # update transition table
+        transs.setdefault(state, {}).setdefault(bestAction, {}).setdefault(newState, 0)
+        transs[state][bestAction][newState] += 1
 
-	The algorithm returns the number of iterations
-	needed to reach a terminal state
-	"""
-	itr = 0
-	isTerminal = False
-	state = env.getStartingState()
-	
+        actions = env.getActions(newState)
+        rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
 
-	actions = env.getActions(state)
-	rewardEstimate, bestAction = None, None
-	if len(utils) > 0: # if this is not the first trial
-		rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
+        # Update utility
+        utils[state] = reward + _alpha(freqs.get(state, 0)) * rewardEstimate
 
-	while not isTerminal: # while not terminal
-		t = len(freqs) or 1
-		if random.random() < 1./t or bestAction is None:
-			# If it is the first iteration or exploration event
-			# then randomly choose an action
-			bestAction = random.choice(actions)
-		
-		# do the action with the best policy
-		# or do some random exploration
-		
-		newState, reward, isTerminal = env.do(state, bestAction)
-		freqs.setdefault(newState, 0)
-		freqs[newState] += 1
+        state = newState
+        itr = itr + 1
+        if itr >= maxItr:
+            break
+    return itr
 
-		env.printState(state)
-		print actions, bestAction, isTerminal
-		env.printState(newState)
-		print "----------"
-		print "----------"
-		print "----------"
-
-		# update transition table
-		transs.setdefault(state, {}).setdefault(bestAction, {}).setdefault(newState, 0)
-		transs[state][bestAction][newState] += 1
-
-		actions = env.getActions(newState)
-		rewardEstimate, bestAction = max(_getEstimates(transs, utils, state, actions))
-		
-		# Update utility
-		utils[state] = reward + _alpha(freqs.get(state, 0)) * rewardEstimate
-		
-		state = newState
-		itr = itr+1
-		if itr >= maxItr:
-			break
-	return itr
-
+# Agent class.
 class Agent():
+    def __init__(self):
+        self.clearExperience()
 
-	def __init__(self):
-		self.clearExperience()
+    def clearExperience(self):
+    # Frequency table.
+        self.nTable = {}
 
-	def clearExperience(self):
-		self.nTable = {}
-		self.transTable = {}
-		self.uTable = {}
+        # Transition table.
+        self.transTable = {}
 
-	def getPolicy(self):
-		policy = {}
-		for state in self.transTable:
-			policy[state] = max(_getEstimates(self.transTable, self.uTable, state))[1]
-		return policy
+        # Utilities table.
+        self.uTable = {}
 
-	def learn(self, env, alg=adp_random_exploration, numOfTrials=100):
-		itrs = 0
-		for trial in range(numOfTrials):
-			itrs += alg(env, transs=self.transTable, 
-						utils=self.uTable, 
-						freqs=self.nTable)
-		return self.getPolicy()
+    def getPolicy(self):
+        policy = {}
+        # For every state set appropriate action.
+        for state in self.transTable:
+            policy[state] = max(_getEstimates(self.transTable, self.uTable, state))[1]
+        return policy
 
-	def solve(self, env, policy):
-		# solve environment with respect to policy
-		actions, energy = [], 0
-		state, prevState = env.getStartingState(), None
-		isTerminalState = False
-		while not isTerminalState:
-			act = policy.get(state)
-			if act is None:
-				act = random.choice(env.getActions(state))
-			state, reward, isTerminalState = env.do(state, act)
-			
-			actions.append(act)
-			energy += reward
-		return actions, energy
+        # Learn best policy given the environment, algorithm and number of trials.
+
+    def learn(self, env, alg=adp_random_exploration, numOfTrials=100):
+        itrs = 0
+        for trial in range(numOfTrials):
+            itrs += alg(env, transs=self.transTable,
+                        utils=self.uTable,
+                        freqs=self.nTable)
+        return self.getPolicy()
+
+    def solve(self, env, policy):
+        # solve environment with respect to policy
+        actions, energy = [], 0
+
+        # Set state to starting state of environment.
+        state, prevState = env.getStartingState(), None
+        isTerminalState = False
+        while not isTerminalState:
+        # Policy has best actions for given state.
+            act = policy.get(state)
+            if act is None:
+                act = random.choice(env.getActions(state))
+                # Execute selected action in current state.
+            state, reward, isTerminalState = env.do(state, act)
+
+            actions.append(act)
+            energy += reward
+            # We get a list of actions that were executed and sum of rewards that were given when agent entered certain state.
+        return actions, energy
 
--- environments.py
+++ environments.py
@@ -1,192 +1,228 @@
 class Environment():
-	def __init__(self):
-		raise NotImplementedError("__init__")
-	
-	def getStartingState(self):
-		raise NotImplementedError("getStartingState")
+    def __init__(self):
+        raise NotImplementedError("__init__")
 
-	def do(self, state, action):
-		raise NotImplementedError("do")
+    def getStartingState(self):
+        raise NotImplementedError("getStartingState")
 
-	def getActions(self, state):
-		raise NotImplementedError("getActions")
+    def do(self, state, action):
+        raise NotImplementedError("do")
+
+    def getActions(self, state):
+        raise NotImplementedError("getActions")
 
 class Sokoban(Environment):
-	def __init__(self, size=(5, 5), 
-				 agentPos=(0, 0), 
-				 boxPosList=[], 
-				 endPosList=[], 
-				 holePosList=[],
-				 stonePosList=[]):
-		self.size = size
-		self.stonePosSet = set(stonePosList) # optimizes search
+    def __init__(self, size=(5, 5),
+                 agentPos=(0, 0),
+                 boxPosList=[],
+                 endPosList=[],
+                 holePosList=[],
+                 stonePosList=[]):
+        self.size = size
+        self.stonePosSet = set(stonePosList) # optimizes search
 
         # Start position is our position + positions of boxes.
         # Position is define for dynamic objects, in this case agent and boxes.
-		self.startPos = (agentPos, ) + tuple(boxPosList)
+        self.startPos = (agentPos, ) + tuple(boxPosList)
 
         # Terminal positions or positions to which the boxes need to be moved.
-		self.endPosSet = set(endPosList)
+        self.endPosSet = set(endPosList)
 
         # Hole?
-		self.holePosSet = set(holePosList)
+        self.holePosSet = set(holePosList)
 
         # Using notation of first columns then rows.
-		self.possibleActions = [
-			(0, +1), # up
-			(0, -1), # down
-			(-1, 0), # left
-			(+1, 0), # right
-		]
-		self.possibleActionsDict = {
-			"up": self.possibleActions[0],
-			"down": self.possibleActions[1],
-			"left": self.possibleActions[2],
-			"right": self.possibleActions[3],
-		}
+        self.possibleActions = [
+            (0, +1), # up
+            (0, -1), # down
+            (-1, 0), # left
+            (+1, 0), # right
+        ]
+        self.possibleActionsDict = {
+            "up": self.possibleActions[0],
+            "down": self.possibleActions[1],
+            "left": self.possibleActions[2],
+            "right": self.possibleActions[3],
+        }
 
         # Count for end position.
-		self.endOnEdgeCount = sum(self._on_edge(loc) for loc in self.endPosSet)
+        self.endOnEdgeCount = sum(self._on_edge(loc) for loc in self.endPosSet)
 
-	def printState(self, state):
+    def printState(self, state):
         # In the beginning state is self.startPos!
-		agentPos = state[0]
-		boxPosSet = set(state[1:]) # set optimizes search
+        agentPos = state[0]
+        boxPosSet = set(state[1:]) # set optimizes search
 
         # Print columns before last row.
-		print "|" + "".join("-" for i in range(self.size[0])) + "|"
+        print "|" + "".join("-" for i in range(self.size[0])) + "|"
 
         # Over columns.
-		for j in range(self.size[0]):
-			line = "|"
+        for j in range(self.size[0]):
+            line = "|"
 
             # Over rows.
-			for i in range(self.size[1]):
+            for i in range(self.size[1]):
                 # Change column and row order.
-				pt = (i, self.size[1] - 1 - j)
-				if pt == agentPos:
-					line += "A"
-				elif pt in boxPosSet:
-					line += "B"
-				elif pt in self.endPosSet:
-					line += "*"
-				elif pt in self.holePosSet:
-					line += "o"
-				elif pt in self.stonePosSet:
-					line += "#"
-				else:
-					line += " "
-			print line + "|"
+                pt = (i, self.size[1] - 1 - j)
+                if pt == agentPos:
+                    line += "A"
+                elif pt in boxPosSet:
+                    line += "B"
+                elif pt in self.endPosSet:
+                    line += "*"
+                elif pt in self.holePosSet:
+                    line += "o"
+                elif pt in self.stonePosSet:
+                    line += "#"
+                else:
+                    line += " "
+            print line + "|"
 
         # Print columns after last row.
-		print "|" + "".join("-" for i in range(self.size[0])) + "|"
+        print "|" + "".join("-" for i in range(self.size[0])) + "|"
 
 
-	def getStartingState(self):
-		return self.startPos
+    def getStartingState(self):
+        return self.startPos
 
-	def do(self, state, action):
+    def do(self, state, action):
         # Select appropriate action given notation.
-		if action in self.possibleActionsDict:
-			action = self.possibleActionsDict[action]
+        if action in self.possibleActionsDict:
+            action = self.possibleActionsDict[action]
 
-		agentPos = state[0]
-		boxPosSet = set(state[1:]) # set optimizes search
-		newPos = self._add(agentPos, action) # get new position with respect to action
-		boxList = [] # new box positions
-		reward = -10 # agent reward
-		isTerminalState = False
+        agentPos = state[0]
+        boxPosSet = set(state[1:]) # set optimizes search
+        newPos = self._add(agentPos, action) # get new position with respect to action. We have already checked whether this action is possible.
+        boxList = [] # new box positions
+        reward = -10 # agent reward. For evey additional move the agent gets negative points.
+        isTerminalState = False
+        newBoxPos = None;
 
-		# After player move check positions for boxes.
-		for boxPos in boxPosSet:
-			newBoxPos = boxPos
-			# check if new position moves a box
-			if boxPos == newPos:
+        # After player move check positions for boxes. Do we have to iterate to the end over all the boxes?
+        # Only one box can be moved at a time so we could break out early.
+        for boxPos in boxPosSet:
+            # check if new position moves a box
+            if boxPos == newPos:
                 # Move box.
-				newBoxPos = self._add(boxPos, action)
-				reward = 30
-			boxList.append(newBoxPos)
+                newBoxPos = self._add(boxPos, action)
+                reward = 30
+                boxList.append(newBoxPos)
+            boxList.append(boxPos)
+        # IN THIS SECTION WE SHOULD FIRST TRY TO FINDDEADLOCKS!
+        # There are three papers:
+        # http://www.lamsade.dauphine.fr/~cazenave/papers/sokoban.pdf
+        # http://webdocs.cs.ualberta.ca/~jonathan/publications/ai_publications/ai98_soko.pdf
+        # http://weetu.net/Timo-Virkkala-Solving-Sokoban-Masters-Thesis.pdf
+        # I am inclining towards last (master thesis), where on page 44 there is a description
+        # of simple deadlock detection in O(1) time.
+
+        # Check for situations where box cannot be moved any more to any of the possible end positions.
+        # First we start with corner position.
+        if newBoxPos is not None and newBoxPos not in self.endPosSet and self._in_corner(newBoxPos):
+            reward = -1000
+            isTerminalState = True
 
         # How many boxes are in end position and on edge of environment?
-		boxInEndPosCount = sum(box in self.endPosSet for box in boxList)
-		boxOnEdgeCount = sum(self._on_edge(loc) for loc in boxList)
-		# Check if the number of boxes on edges
-		# exceeds number of ends on edge
-		if boxOnEdgeCount > self.endOnEdgeCount:
-			# When a box is on the edge it cannot be moved to the center
-			# and that is why the game is over
-			# TODO: Jernej, check if this is a fact
+        boxInEndPosCount = sum(box in self.endPosSet for box in boxList)
+        boxOnEdgeCount = sum(self._on_edge(loc) for loc in boxList)
+        # Check if the number of boxes on edges
+        # exceeds number of ends on edge
+        if boxOnEdgeCount > self.endOnEdgeCount:
+            # When a box is on the edge it cannot be moved to the center
+            # and that is why the game is over
+            # TODO: Jernej, check if this is a fact
             # This is true. You cannot move the box away from edge but you can move it along the edge.
             # And this of course only applies if you are not at the corner. Then you are stuck.
             # TODO: But we should check for every edge (four edges) if the number of
             # TODO: end positions for each edge does not exceed the number of boxes in each edge.
-			reward = -1000
-			isTerminalState = True
+            reward = -1000
+            isTerminalState = True
 
-		# check if we are finished
-		if boxInEndPosCount == len(self.endPosSet):
-			reward = 1000
-			isTerminalState = True
+        # check if we are finished
+        if boxInEndPosCount == len(self.endPosSet):
+            reward = 1000
+            isTerminalState = True
         # First position is new position of a player.
-		return ((newPos, ) + tuple(boxList), 
-				reward, 
-				isTerminalState, )
+        return ((newPos, ) + tuple(boxList),
+                reward,
+                isTerminalState, )
 
     # Inside borders of box. Rename to location for clarity.
-	def _in_borders(self, loc):
-		return 0 <= loc[0] < self.size[0] and 0 <= loc[1] < self.size[1]
+    def _in_borders(self, loc):
+        return 0 <= loc[0] < self.size[0] and 0 <= loc[1] < self.size[1]
 
-	def _on_edge(self, loc):
-		"""
-		Check if location loc=(x,y) is on environment edge
-		"""
+    def _in_corner(self, box):
+        """
+        Check if box is in corner. There can be more than four corners
+        in environment! That is why this function checks corners only
+        for single box in next order (NE = up-right, SE = right-down, SW = down-left,
+        NW = left-up). This can be helpful as in if box is in corner but
+        not in position, then the game cannot be solved.
+        """
+        return self._add(box, self.possibleActions[0]) in self.stonePosSet and self._add(box, self.possibleActions[3]) in self.stonePosSet or \
+               self._add(box, self.possibleActions[3]) in self.stonePosSet and self._add(box, self.possibleActions[1]) in self.stonePosSet or \
+               self._add(box, self.possibleActions[1]) in self.stonePosSet and self._add(box, self.possibleActions[2]) in self.stonePosSet or \
+               self._add(box, self.possibleActions[2]) in self.stonePosSet and self._add(box, self.possibleActions[0]) in self.stonePosSet
+
+    def _on_edge(self, loc):
+        """
+        Check if location loc=(x,y) is on environment edge
+        """
 
         # Columns and rows count starts at 0!
-		return sum((loc[i] in {0, self.size[i]-1}) for i in range(2)) > 0
+        return sum((loc[i] in {0, self.size[i]-1}) for i in range(2)) > 0
 
-	def _add(self, absolute, relative):
+    def _add(self, absolute, relative):
         # Absolute = agent position or box position, relative = executed action (i.e. (0, +1))
-        # Checking if agent moves out of the box is done outside.
-		return (absolute[0] + relative[0], absolute[1] + relative[1])
+        # Checking if agent moves out of the environment is done outside.
+        return (absolute[0] + relative[0], absolute[1] + relative[1])
 
-	def getActions(self, state):
-		agentPos = state[0]
-		boxPosSet = set(state[1:]) # set optimizes search
-		
-		# check if possible
-		actions = []
+    def getActions(self, state):
+        agentPos = state[0]
+        boxPosSet = set(state[1:]) # set optimizes search
+
+        # check if possible
+        actions = []
 
         # Possible actions (0, +1), (0, -1), etc.
-		for diff in self.possibleActions:
-			newPos = self._add(agentPos, diff)
-			if not self._in_borders(newPos) or newPos in self.stonePosSet:
-				continue
-			if newPos in boxPosSet: # we are moving a box
-				newBoxPos = self._add(newPos, diff)
+        for diff in self.possibleActions:
+            newPos = self._add(agentPos, diff)
+            if not self._in_borders(newPos) or newPos in self.stonePosSet:
+                continue
+            if newPos in boxPosSet: # we are moving a box
+                newBoxPos = self._add(newPos, diff)
 
                 # Remove position of the moved box. The position matches the new position of agent.
-				otherBoxes = boxPosSet - {newPos, } # get other boxes
-				if newBoxPos in otherBoxes:
-					# you cannot move two boxes in same time
-					continue
-				if not self._in_borders(newBoxPos) or newBoxPos in self.stonePosSet:
-					continue
-			# everything is OK
-			actions.append(diff) # TODO: check if absolute actions are better
+                otherBoxes = boxPosSet - {newPos, } # get other boxes
+                if newBoxPos in otherBoxes:
+                    # you cannot move two boxes in same time
+                    continue
+                if not self._in_borders(newBoxPos) or newBoxPos in self.stonePosSet:
+                    continue
+            # everything is OK
+            actions.append(diff) # TODO: check if absolute actions are better
         # Returns all possible actions for current state of environment.
-		return actions
-	
+        return actions
+
 
 simple1 = Sokoban((5,5), 
-				  agentPos=(1,1), 
-				  boxPosList=[(1,2)], 
-				  endPosList=[(2,1)], 
-				  holePosList=[], 
-				  stonePosList=[(2, 2)])
+                  agentPos=(1,1),
+                  boxPosList=[(1,2)],
+                  endPosList=[(2,1)],
+                  holePosList=[],
+                  stonePosList=[(2, 2)])
 
 simple2 = Sokoban((10, 10), 
-				  agentPos=(1,1), 
-				  boxPosList=[(1,2)], 
-				  endPosList=[(2,1)], 
-				  holePosList=[], 
-				  stonePosList=[(2, 2)])
+                  agentPos=(1,1),
+                  boxPosList=[(1,2)],
+                  endPosList=[(2,1)],
+                  holePosList=[],
+                  stonePosList=[(2, 2)])
+
+# Check for corner detection.
+simple3 = Sokoban((5, 5),
+                  agentPos=(0,0),
+                  boxPosList=[(3,3)],
+                  endPosList=[(4,1)],
+                  holePosList=[],
+                  stonePosList=[(2, 2), (2, 3), (3, 2)])
