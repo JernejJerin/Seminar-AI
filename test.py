b11679f74f63b0b923881860339f401d1701e51a
Žiga Stopinšek <sigi.kajzer@gmail.com>
2014-01-08 19:19:33 +0100

    added new test

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ MAX_ITERATIONS = (
 	20, 30, 40
 )
 NUM_OF_TRIALS = 500
-
+NUM_OF_TESTS = 50
 ENVIRONMENTS = (
 	e.simple4,
 	e.simple5,
@@ -22,18 +22,31 @@ ENVIRONMENTS = (
 
 results = {}
 
+success = {}
+
 def test():
+
 	for alg in ALGORITHMS:
 		for env in ENVIRONMENTS:
 			for maxIter in MAX_ITERATIONS:
-				agent = a.Agent()
+				rewards = []
+				for tst in range(NUM_OF_TESTS):
+					agent = a.Agent()
+					agent.learn(env, alg=alg, numOfTrials=NUM_OF_TRIALS, maxItr=maxIter)
+					test_id = (alg.func_name, env.name, maxIter, NUM_OF_TRIALS, tst, )
+				
+					result = agent.solve(env, agent.getPolicy())
+					rewards.append(1 if result[1] > 0 else 0)
 
-				agent.learn(env, alg=alg, numOfTrials=NUM_OF_TRIALS, maxItr=maxIter)
-				test_id = (alg.func_name, env.name, maxIter, NUM_OF_TRIALS, )
-				results[test_id] = agent.solve(env, agent.getPolicy())[1]
+					
+				success[test_id] = float(sum(rewards)) / NUM_OF_TESTS
+				results[test_id] = result
+					
 	
 	from pprint import pprint
-	pprint(results)
+	pprint(success)
 
 if __name__ == '__main__':
-	test()
+	
+		test()
+
